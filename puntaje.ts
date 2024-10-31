2fc93e2d2fe2482caa308e9406f7b4af16ff68d8
Zahira <132922153+Zahirita@users.noreply.github.com>
2024-10-31 09:23:38 -0300

    Update puntaje.ts

--- puntaje.ts
+++ puntaje.ts
@@ -9,35 +9,41 @@ let Tribu_B : puntaje = {
 }
 
 const Handball_Imput_Tribu_A = document.getElementById('Handball_Input_Tribu_A')as HTMLInputElement;
-const Ajedrez_Input_Tribu_A= document.getElementById('Handball_Input_Tribu_A')as HTMLInputElement;
-const Resistencia_Imput_Tribu_A = document.getElementById('Handball_Input_Tribu_A')as HTMLInputElement;
+const Ajedrez_Input_Tribu_A= document.getElementById('Ajedrez_Input_Tribu_A')as HTMLInputElement;
+const Resistencia_Imput_Tribu_A = document.getElementById('Resistencia_Input_Tribu_A')as HTMLInputElement;
 
 const Handball_Imput_Tribu_B = document.getElementById('Handball_Input_Tribu_B')as HTMLInputElement;
-const Ajedrez_Input_Tribu_B= document.getElementById('Handball_Input_Tribu_B')as HTMLInputElement;
-const Resistencia_Imput_Tribu_B = document.getElementById('Handball_Input_Tribu_B')as HTMLInputElement;
+const Ajedrez_Input_Tribu_B= document.getElementById('Ajedrez_Input_Tribu_B')as HTMLInputElement;
+const Resistencia_Imput_Tribu_B = document.getElementById('Resistencia_Input_Tribu_B')as HTMLInputElement;
 
 const OutPutPuntajeA = document.getElementById('PuntajeA') as HTMLInputElement;
-const OutPutPuntajeB = document.getElementById('PuntajeA') as HTMLInputElement;
+const OutPutPuntajeB = document.getElementById('PuntajeB') as HTMLInputElement;
 
 const TribuGanadora = document.getElementById('EquipoGanador') as HTMLInputElement;
 
-function contar(): void {
-    let Valor_A_Ingresado_Resistencia: number = parseInt(Resistencia_Imput_Tribu_A.value, 10);
-    let Valor_A_Ingresado_Ajedrez: number = parseInt(Ajedrez_Input_Tribu_A.value, 10);
-    let Valor_A_Ingresado_Handball: number = parseInt(Handball_Imput_Tribu_A.value, 10);
+function sumar(): void {
+    let Valor_A_Ingresado_Resistencia: number = parseInt(Resistencia_Imput_Tribu_A.value, 10) || 0;
+    let Valor_A_Ingresado_Ajedrez: number = parseInt(Ajedrez_Input_Tribu_A.value, 10) || 0;
+    let Valor_A_Ingresado_Handball: number = parseInt(Handball_Imput_Tribu_A.value, 10) || 0;
 
-    let Valor_B_Ingresado_Resistencia: number = parseInt(Resistencia_Imput_Tribu_B.value, 10); 
-    let Valor_B_Ingresado_Ajedrez: number = parseInt(Ajedrez_Input_Tribu_B.value, 10);
-    let Valor_B_Ingresado_Handball: number = parseInt(Handball_Imput_Tribu_B.value, 10);
+    let Valor_B_Ingresado_Resistencia: number = parseInt(Resistencia_Imput_Tribu_B.value, 10) || 0; 
+    let Valor_B_Ingresado_Ajedrez: number = parseInt(Ajedrez_Input_Tribu_B.value, 10) || 0;
+    let Valor_B_Ingresado_Handball: number = parseInt(Handball_Imput_Tribu_B.value, 10) || 0;
 
     Tribu_A.puntos += Valor_A_Ingresado_Resistencia + Valor_A_Ingresado_Ajedrez + Valor_A_Ingresado_Handball;
     Tribu_B.puntos += Valor_B_Ingresado_Resistencia + Valor_B_Ingresado_Ajedrez + Valor_B_Ingresado_Handball;
 
-    OutPutPuntajeA.innerText += Tribu_A.puntos.toString()
-    OutPutPuntajeB.innerText += Tribu_B.puntos.toString()
+    OutPutPuntajeA.innerText += Tribu_A.puntos.toString();
+    OutPutPuntajeB.innerText += Tribu_B.puntos.toString();
 
-    if (Tribu_A.puntos>Tribu_B.puntos){
+    if (Tribu_A.puntos > Tribu_B.puntos) {
         TribuGanadora.innerText += 'Tribu_A';
+    }
+
+    else if (Tribu_A.puntos < Tribu_B.puntos) {
+        TribuGanadora.innerText += 'Tribu_B';
+    } else {
+        TribuGanadora.innerText += 'Empate';
     }
 
 }
